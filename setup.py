0ec8e0b9b628be460763c7f18fa4e81cc6078112
Kirill Klenov <horneds@gmail.com>
2021-06-10 13:24:58 +0300

    build: wip

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup
+"""Setup the package."""
 
-setup()
+from setuptools import setup
+from pathlib import Path
+
+reqs = Path(__file__).parent / 'requirements.txt'
+
+setup(
+    install_requires=[ln for ln in reqs.read_text().split('\n') if ln and not ln.startswith('#')]
+)

8a7b3a8c42415d513d35aeed7e4e4c5e9230638d
Kirill Klenov <horneds@gmail.com>
2023-03-03 19:01:52 +0700

    feat: wip

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@
 
 from pathlib import Path
 
+import pkg_resources
 from setuptools import setup
 
 reqs = Path(__file__).parent / "requirements.txt"
 dev_reqs = Path(__file__).parent / "requirements-dev.txt"
 
+
+def parse_requirements(path: str) -> "list[str]":
+    with Path(path).open() as requirements:
+        return [str(req) for req in pkg_resources.parse_requirements(requirements)]
+
+
 setup(
-    install_requires=[
-        ln for ln in reqs.read_text().split("\n") if ln and not ln.startswith("#")
-    ],
-    extras_require={
-        "dev": ln
-        for ln in dev_reqs.read_text().split("\n")
-        if ln and not ln.startswith("#")
-    },
+    install_requires=parse_requirements("requirements.txt"),
+    extras_require={"dev": parse_requirements("requirements-dev.txt")},
 )
